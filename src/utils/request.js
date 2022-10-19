3517d1a9f439fe1c75ec66813e71a114ed86a1e4
moka12579 <71615766+moka12579@users.noreply.github.com>
2022-10-19 16:47:51 +0800

    修复bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import store from "@/store";
 
 const baseURL = process.env.NODE_ENV === "development" ? "/dev-api" : "https://d33a5037-6c63-4e92-8bb1-30018ab701ea.bspapp.com/http"
 
